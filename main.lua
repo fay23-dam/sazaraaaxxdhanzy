5f6f11ec7e3fabe98406e606948a7ad9a41067e7
Sazara <98327214+fay23-dam@users.noreply.github.com>
2026-03-16 02:43:15 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local games = {
     [130342654546662] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/sambung-kata.lua", -- sambungkata
     [83369512629707] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/sawah-indo.lua", -- sawahindo
-    [83369512629707] = "https://raw.githubusercontent.com/dhannxsazaraxhub/testajamok/refs/heads/main/lengkapi-kata.lua", -- lengkapikata
+    [72774564502867] = "https://raw.githubusercontent.com/dhannxsazaraxhub/testajamok/refs/heads/main/lengkapi-kata.lua", -- lengkapikata
     [103159798148004] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/empang-indo.lua", --empangindo
 }
 
