59b9169aeef9fe5c2bcacd1606253d2b597c8025
Sazara <98327214+fay23-dam@users.noreply.github.com>
2026-03-19 03:40:46 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ local games = {
     [83369512629707] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/sawah-indo.lua", -- sawahindo
     [72774564502867] = "https://raw.githubusercontent.com/dhannxsazaraxhub/testajamok/refs/heads/main/lengkapi-kata.lua", -- lengkapikata
     [103159798148004] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/empang-indo.lua", --empangindo
-    [125810438250765] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/deadly-delivers.lua"
+    [8950496606] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/deadly-delivers.lua"
 }
 
 local currentID = game.PlaceId
