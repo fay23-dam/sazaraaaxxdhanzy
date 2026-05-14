707bee17e2cb8b8530ba2c09399202e82be4e3c3
Sazara <98327214+fay23-dam@users.noreply.github.com>
2026-05-15 01:55:33 +0900

    Auto-update: sync scripts from admin panel

--- main.lua
+++ main.lua
@@ -1,12 +1,11 @@
 local games = {
-    [130342654546662] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/sambung-kata.lua", -- sambungkata
-    [83369512629707] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/sawah-indo.lua", -- sawahindo
-    [72774564502867] = "https://raw.githubusercontent.com/dhannxsazaraxhub/testajamok/refs/heads/main/lengkapi-kata.lua", -- lengkapikata
-    [103159798148004] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/empang-indo.lua", --empangindo
-    [125810438250765] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/deadly-delivers.lua",
-    [93044798454681]  = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/deadly-delivers.lua",
-    [114640202062357]  = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/swingforabrainrot.lua",
-    [18680867089] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/ultimate-mining-tycoon.lua", -- ultimate mining tycoon
+    [433443334433] = "https://raw.githubusercontent.com/D4nzyXcripter/luraphdeobfluascriptvictoria/refs/heads/main/index.html?token=GHSAT0AAAAAAD3Y5AOX6PJAYHKTE5HCFHW22QF5AJA", -- hkdfhah
+    [83369512629707] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/sawah-indo.lua", -- Sawah Indo
+    [72774564502867] = "https://raw.githubusercontent.com/dhannxsazaraxhub/testajamok/refs/heads/main/lengkapi-kata.lua", -- Lengkapi Kata
+    [125810438250765] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/deadly-delivers.lua", -- Deadly Delivery
+    [103159798148004] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/empang-indo.lua", -- Empang Indo
+    [18680867089] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/ultimate-mining-tycoon.lua", -- Ultimate Mining Tycoon
+    [114640202062357] = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/swingforabrainrot.lua", -- Swing Obby for a Brainrot
 }
 
 local currentID = game.PlaceId
