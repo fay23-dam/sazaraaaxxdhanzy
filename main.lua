680c866680d972bc2243f457f9e575786621e087
Sazara <98327214+fay23-dam@users.noreply.github.com>
2026-07-12 20:25:21 +0900

    Auto-update: sync scripts from admin panel

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 local games = {
+    [9897663111] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/main/backstreet-survival.lua", status = "active" }, -- Backstreet Survival
     [79268393072444] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/main/sell-lemons-.lua", status = "active" }, -- Sell Lemons!
     [82524183928567] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/main/build-a-soccer-squad-.lua", status = "active" }, -- Build A Soccer Squad!
     [10334731049] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/main/clean-the-supermarket.lua", status = "active" }, -- clean the supermarket
@@ -8,11 +9,11 @@ local games = {
     [140209132616484] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/main/water-empire.lua", status = "active" }, -- Water Empire
     [114115486822297] = { url = "https://pastefy.app/Ksz9zj49/raw", status = "disabled" }, -- Spin A Lucky Block For Soccer Cards
     [8950496606] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/main/deadly-delivery.lua", status = "active" }, -- Deadly Delivery
-    [103159798148004] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/empang-indo.lua", status = "disabled" }, -- Empang Indo
-    [130342654546662] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/sambung-kata.lua", status = "disabled" }, -- Sambung Kata
-    [18680867089] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/ultimate-mining-tycoon.lua", status = "active" }, -- Ultimate Mining Tycoon
-    [114640202062357] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/swingforabrainrot.lua", status = "active" }, -- Swing Obby for a Brainrot
     [72774564502867] = { url = "https://raw.githubusercontent.com/dhannxsazaraxhub/testajamok/refs/heads/main/lengkapi-kata.lua", status = "stable" }, -- Lengkapi Kata
+    [114640202062357] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/swingforabrainrot.lua", status = "active" }, -- Swing Obby for a Brainrot
+    [18680867089] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/ultimate-mining-tycoon.lua", status = "active" }, -- Ultimate Mining Tycoon
+    [130342654546662] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/sambung-kata.lua", status = "disabled" }, -- Sambung Kata
+    [103159798148004] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/empang-indo.lua", status = "disabled" }, -- Empang Indo
     [83369512629707] = { url = "https://raw.githubusercontent.com/fay23-dam/sazaraaax-script/refs/heads/main/sawah-indo.lua", status = "stable" }, -- Sawah Indo
 }
 
